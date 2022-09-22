7081722cc85353b9416705a5f6440e717be36875
Sahil Gupta <s_gupta@berkeley.edu>
2022-09-22 00:45:10 -0700

    Add health system and health bar, game manager.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        } else if (Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D p_RigidBody;
 
     private const float p_Speed = 10.0f;
+
+    private float p_MaxHealth = 5f;
+    private float p_CurrHealth;
+    private Slider p_HealthBar;
     #endregion
 
     #region Initialization
@@ -15,6 +20,10 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         p_RigidBody = GetComponent<Rigidbody2D>();
+        p_CurrHealth = p_MaxHealth;
+        p_HealthBar = GameObject.FindGameObjectWithTag("Health").GetComponent<Slider>();
+        p_HealthBar.maxValue = p_MaxHealth;
+        p_HealthBar.value = p_CurrHealth;
     }
     #endregion
 
@@ -26,6 +35,22 @@ public class PlayerController : MonoBehaviour
 
         Vector2 moveDirection = new Vector2(xAxis, yAxis);
         p_RigidBody.velocity = moveDirection * p_Speed;
+    }
+
+    public void UpdateHealth(float val)
+    {
+        p_CurrHealth = Mathf.Min(Mathf.Max(0f, p_CurrHealth + val), p_MaxHealth);
+        p_HealthBar.value = p_CurrHealth;
+        // Floating point comparisons are always suspect.
+        if (p_CurrHealth == 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(this.gameObject);
     }
     #endregion
 }
